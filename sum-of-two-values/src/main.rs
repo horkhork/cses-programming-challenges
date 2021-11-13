ad9b003180d31c00ba8793bedf6fee7a12c79843
steve <ssosik@akamai.com>
2021-11-13 15:27:54 -0500

    Partial implementation

--- sum-of-two-values/src/main.rs
+++ sum-of-two-values/src/main.rs
@@ -6,7 +6,7 @@ fn main() {
     let mut line = "".to_string();
     input.read_line(&mut line).unwrap();
     let mut split = line.split_whitespace();
-    let n: i32 = split.next().unwrap().parse().unwrap();
+    let _n: i32 = split.next().unwrap().parse().unwrap();
     let x: i32 = split.next().unwrap().parse().unwrap();
 
     let mut values: BTreeMap<i32, i32> = BTreeMap::new();
@@ -15,18 +15,18 @@ fn main() {
     input.read_line(&mut line).unwrap();
     for (i, val) in line.split_whitespace().enumerate() {
         values.insert(val.parse::<i32>().unwrap(), i as i32);
-        println!("Adding {} at index {}", val, i);
+        //println!("Adding {} at index {}", val, i);
     }
-    println!("n:{} ={} values: {:?}", n, x, values);
+    //println!("n:{} ={} values: {:?}", n, x, values);
     let second_half = values.split_off(&(x/2));
-    println!("First {:?}", values);
-    println!("Second {:?}", second_half);
+    //println!("First {:?}", values);
+    //println!("Second {:?}", second_half);
     for (a, i) in values.iter(){
         //println!("check: {}", a);
         for (b, j) in second_half.iter().rev() {
-            println!("check: a:{}({}) b:{}({})", a, i, b, j);
+            //println!("check: a:{}({}) b:{}({})", a, i, b, j);
             if a + b == x {
-                println!("Found one: a:{}({}) b:{}({})", a, i, b, j);
+                //println!("Found one: a:{}({}) b:{}({})", a, i, b, j);
                 println!("{} {}", i+1, j+1);
                 return;
             }
