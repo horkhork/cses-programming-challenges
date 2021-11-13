4c9bc4d17b75e9337d10f7d1c58d06486e5fae64
steve <ssosik@akamai.com>
2021-11-13 15:07:42 -0500

    sum-of-two-values WIP

--- sum-of-two-values/src/main.rs
+++ sum-of-two-values/src/main.rs
@@ -6,7 +6,7 @@ fn main() {
     let mut line = "".to_string();
     input.read_line(&mut line).unwrap();
     let mut split = line.split_whitespace();
-    let _n: i32 = split.next().unwrap().parse().unwrap();
+    let n: i32 = split.next().unwrap().parse().unwrap();
     let x: i32 = split.next().unwrap().parse().unwrap();
 
     let mut values: BTreeSet<i32> = BTreeSet::new();
@@ -16,7 +16,17 @@ fn main() {
     for val in line.split_whitespace() {
         values.insert(val.parse::<i32>().unwrap());
     }
-    println!("values: {:?}", values);
+    println!("n:{} ={} values: {:?}", n, x, values);
+    let second_half = values.split_off(&(x/2));
+    for (i, a) in values.iter().enumerate() {
+        //println!("check: {}", a);
+        for (j, b) in second_half.iter().enumerate().rev() {
+            println!("check: a:{}({}) b:{}({})", a, i, b, j);
+            if a + b == x {
+                println!("Found one: a:{}({}) b:{}({})", a, i, b, j);
+            }
+        }
+    }
 
     println!("{}", -1);
 }
