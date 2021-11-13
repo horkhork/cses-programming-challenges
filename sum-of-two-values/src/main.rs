50ef8502e4ad8e454305bf2a67df5fbdd6434198
steve <ssosik@akamai.com>
2021-11-13 15:25:47 -0500

    WIP

--- sum-of-two-values/src/main.rs
+++ sum-of-two-values/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::BTreeSet;
+use std::collections::BTreeMap;
 use std::io::{BufRead, BufReader};
 
 fn main() {
@@ -9,26 +9,29 @@ fn main() {
     let n: i32 = split.next().unwrap().parse().unwrap();
     let x: i32 = split.next().unwrap().parse().unwrap();
 
-    let mut values: BTreeSet<i32> = BTreeSet::new();
+    let mut values: BTreeMap<i32, i32> = BTreeMap::new();
 
     let mut line = "".to_string();
     input.read_line(&mut line).unwrap();
-    for val in line.split_whitespace() {
-        values.insert(val.parse::<i32>().unwrap());
+    for (i, val) in line.split_whitespace().enumerate() {
+        values.insert(val.parse::<i32>().unwrap(), i as i32);
+        println!("Adding {} at index {}", val, i);
     }
     println!("n:{} ={} values: {:?}", n, x, values);
     let second_half = values.split_off(&(x/2));
-    let mut second_half: Vec<&i32> = second_half.iter().collect();
-    second_half.reverse();
-    for (i, a) in values.iter().enumerate() {
+    println!("First {:?}", values);
+    println!("Second {:?}", second_half);
+    for (a, i) in values.iter(){
         //println!("check: {}", a);
-        for (j, b) in second_half.iter().enumerate() {
+        for (b, j) in second_half.iter().rev() {
             println!("check: a:{}({}) b:{}({})", a, i, b, j);
-            if a + *b == x {
+            if a + b == x {
                 println!("Found one: a:{}({}) b:{}({})", a, i, b, j);
+                println!("{} {}", i+1, j+1);
+                return;
             }
         }
     }
 
-    println!("{}", -1);
+    println!("IMPOSSIBLE");
 }
