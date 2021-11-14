7877d19ea53c53b8a9f22e3a3077a4ecab506cc1
steve <ssosik@akamai.com>
2021-11-14 11:06:01 -0500

    Getting closer

--- sum-of-two-values/src/main.rs
+++ sum-of-two-values/src/main.rs
@@ -10,15 +10,23 @@ fn main() {
     let x: i32 = split.next().unwrap().parse().unwrap();
 
     let mut values: BTreeMap<i32, i32> = BTreeMap::new();
-
+    let half = x / 2;
+    let mut half_loc1 = None;
     let mut line = "".to_string();
     input.read_line(&mut line).unwrap();
     for (i, val) in line.split_whitespace().enumerate() {
-        values.insert(val.parse::<i32>().unwrap(), i as i32);
+        let val = val.parse::<i32>().unwrap();
+        if val == half && half_loc1.is_none() {
+            half_loc1 = Some(i);
+        } else if val == half {
+            println!("{} {}", half_loc1.unwrap() + 1, i + 1);
+            return;
+        }
+        values.insert(val, i as i32);
         //println!("Adding {} at index {}", val, i);
     }
     //println!("n:{} ={} values: {:?}", n, x, values);
-    let second_half = values.split_off(&(x / 2));
+    let second_half = values.split_off(&(half + 1));
     //println!("First {:?}", values);
     //println!("Second {:?}", second_half);
     for (a, i) in values.iter() {
