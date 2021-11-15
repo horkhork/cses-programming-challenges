9e141d3a83706b4eb9d0e91c3c5df02a9ee1bfa6
steve <ssosik@akamai.com>
2021-11-15 15:39:15 -0500

    WIP

--- apple-division/src/main.rs
+++ apple-division/src/main.rs
@@ -1,12 +1,12 @@
-use std::collections::BTreeSet;
+use std::collections::{BTreeSet, BTreeMap};
 use std::io::{BufRead, BufReader};
 
 fn main() {
     let mut input = BufReader::new(std::io::stdin());
     let mut line = "".to_string();
     input.read_line(&mut line).unwrap();
-    let mut split = line.split_whitespace();
-    let _n: i32 = split.next().unwrap().parse().unwrap();
+    //let mut split = line.split_whitespace();
+    //let _n: i32 = split.next().unwrap().parse().unwrap();
 
     let mut sum = 0;
     let mut values: BTreeSet<i32> = BTreeSet::new();
@@ -17,50 +17,26 @@ fn main() {
         values.insert(val);
         sum += val;
     }
-    println!("{} values: {:?}", sum, values);
-    let part1 = sum / 2;
-    let part2 = sum - part1;
-    println!("{} {}", part1, part2);
-    //
-    println!("{}", part2- part1);
+    //println!("{} values: {:?}", sum, values);
 
-    let mut s = -1;
-    //let to_remove = 0;
-    for val in values.iter().rev() {
-        if s + val <= part2 {
-            s += val;
-            println!("add {}; {}", val, s);
-        } else {
-            println!("nope {}", val);
-        }
-        //to_remove =
-    }
     let ret = subset_sums(values.into_iter().collect(), sum / 2, vec![]);
-    //for (a, i) in values.iter() {
-    //    //println!("check: {}", a);
-    //    if let Some(j) = second_half.get(&(x - a)) {
-    //        println!("{} {}", i + 1, j + 1);
-    //        return;
-    //    };
-    //}
-
+    let lowest = ret.iter().next().unwrap();
+    println!("{}", lowest.0);
+    println!("{:?}", ret);
 }
 
-
-enum Possibilities {
-    Lt,
-    Eq,
-    Gt,
-}
-
-fn subset_sums(numbers: Vec<i32>, threshold: i32, part: Vec<i32>) -> Possibilities {
+fn subset_sums(numbers: Vec<i32>, threshold: i32, part: Vec<i32>) -> BTreeMap<i32, Vec<i32>> {
     let s: i32 = part.iter().sum();
+    let mut counts: BTreeMap<i32, Vec<i32>> = BTreeMap::new();
     if s == threshold {
         println!("EQ threshold {} with {}", threshold, s);
-        return Possibilities::Eq;
+        counts.insert(0, part);
+        return counts;
     } else if s > threshold {
-        //println!("Exceeded threshold {} with {}", threshold, s);
-        return Possibilities::Gt;
+        println!("Exceeded threshold {} with {}", threshold, s);
+        let over = s - threshold;
+        counts.insert(over, part);
+        return counts;
     }
 
     for (i, n) in numbers.iter().enumerate() {
@@ -69,9 +45,11 @@ fn subset_sums(numbers: Vec<i32>, threshold: i32, part: Vec<i32>) -> Possibiliti
         let mut p = Vec::new();
         p.extend_from_slice(&part);
         p.push(*n);
-        subset_sums(rem, threshold, p);
-        println!("list {} {} {:?}+{}", (part.iter().sum::<i32>() + n)-threshold, threshold, part, n);
+        let mut ret = subset_sums(rem, threshold, p);
+        counts.append(&mut ret);
+
+        //println!("list {} {} {:?}+{}", (part.iter().sum::<i32>() + n)-threshold, threshold, part, n);
     }
 
-    return Possibilities::Lt;
+    return counts;
 }
