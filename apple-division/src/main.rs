76181a4332b4d6674ebc8b2065b60baf46a6b9a1
steve <ssosik@akamai.com>
2021-11-15 11:43:26 -0500

    WIP

--- apple-division/src/main.rs
+++ apple-division/src/main.rs
@@ -17,13 +17,25 @@ fn main() {
         values.insert(val);
         sum += val;
     }
-    //println!("{} values: {:?}", sum, values);
+    println!("{} values: {:?}", sum, values);
     let part1 = sum / 2;
     let part2 = sum - part1;
-    //println!("{} {}", part1, part2);
+    println!("{} {}", part1, part2);
     //
     println!("{}", part2- part1);
 
+    let mut sum = -1;
+    let to_remove = 0;
+    while values.len() {
+    for val in values.iter().rev() {
+        if sum + val <= part2 {
+            sum += val;
+            println!("add {}; {}", val, sum);
+        } else {
+            println!("nope {}", val);
+        }
+        to_remove =
+    }
     //for (a, i) in values.iter() {
     //    //println!("check: {}", a);
     //    if let Some(j) = second_half.get(&(x - a)) {
