0a49070d97ec90437ec7faffd9cefa8e263601fd
steve <ssosik@akamai.com>
2021-11-23 09:01:06 -0500

    Working on iterator list permutations

--- missing-coin-sum/src/main.rs
+++ missing-coin-sum/src/main.rs
@@ -10,19 +10,30 @@ fn main() {
     let mut line = "".to_string();
     input.read_line(&mut line).unwrap();
 
-    //let values: Vec<i64> = line.split_whitespace().filter_map(|v| v.parse::<i64>().ok()).collect();
-    let best = line.split_whitespace().filter_map(|v| v.parse::<i64>().ok())
-        //.iter()
-        .fold((i64::MIN, 0), |(mut best, mut current), x| {
-            if current <= 0 {
-                current = x;
-            } else {
-                current += x;
-            }
-            if current > best {
-                best = current;
-            }
-            (best, current)
-        });
-    println!("{:?}", best.0);
+    let values: Vec<i64> = line
+        .split_whitespace()
+        .filter_map(|v| v.parse::<i64>().ok())
+        .collect();
+    let v2 = values.clone();
+
+    values
+        .iter()
+        .flat_map(|x| v2.iter().map(|y| vec![x, y]).collect::<Vec<Vec<&i64>>>())
+        .inspect(|v| println!("V{:?}", v))
+        .collect::<Vec<Vec<&i64>>>();
+    ////let values: Vec<i64> = line.split_whitespace().filter_map(|v| v.parse::<i64>().ok()).collect();
+    //let best = line.split_whitespace().filter_map(|v| v.parse::<i64>().ok())
+    //    //.iter()
+    //    .fold((i64::MIN, 0), |(mut best, mut current), x| {
+    //        if current <= 0 {
+    //            current = x;
+    //        } else {
+    //            current += x;
+    //        }
+    //        if current > best {
+    //            best = current;
+    //        }
+    //        (best, current)
+    //    });
+    //println!("{:?}", best.0);
 }
