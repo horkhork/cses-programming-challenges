5f3763ed1ef1410cd8160374b02197aa1e08dc02
steve <ssosik@akamai.com>
2021-11-24 08:04:06 -0500

    Solved it

--- collecting-numbers/src/main.rs
+++ collecting-numbers/src/main.rs
@@ -1,32 +1,28 @@
+use std::collections::BTreeSet;
 use std::io::{BufRead, BufReader};
 
 fn main() {
     let mut input = BufReader::new(std::io::stdin());
     let mut line = "".to_string();
     input.read_line(&mut line).unwrap();
-    let n: i32 = line.split_whitespace().next().unwrap().parse().unwrap();
+    let _n: i32 = line.split_whitespace().next().unwrap().parse().unwrap();
 
     let mut line = "".to_string();
     input.read_line(&mut line).unwrap();
 
-    let values: Vec<i64> = line
+    let mut set: BTreeSet<i64> = BTreeSet::new();
+
+    let cnt: usize = line
         .split_whitespace()
         .filter_map(|v| v.parse::<i64>().ok())
-        .collect();
-
-    let mut cnt = 0;
-    let mut i = 1;
-    while i <= n {
-        let mut in_range = false;
-        for x in values.clone() {
-            if i == x as i32 {
-                if !in_range {
-                    cnt += 1;
-                    in_range = true;
-                }
-                i += 1;
+        .map(|x| {
+            set.insert(x);
+            match set.contains(&(x - 1)) {
+                true => 0,
+                false => 1,
             }
-        }
-    }
+        })
+        .sum();
+
     println!("{}", cnt);
 }
