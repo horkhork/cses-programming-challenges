2c33772ce404ac3cbcb67141ded2dacde407adf7
steve <ssosik@akamai.com>
2021-11-27 14:54:29 -0500

    Initial commit

---
+++ playlist/src/main.rs
@@ -0,0 +1,46 @@
+use std::collections::BTreeMap;
+use std::io::{BufRead, BufReader};
+use std::cmp;
+
+fn main() {
+    let input = BufReader::new(std::io::stdin());
+    let mut lines = input.lines();
+
+    // Parse the first line, unused
+    let (_n, ) = match lines
+        .next()
+        .unwrap()
+        .unwrap()
+        .split_whitespace()
+        .filter_map(|v| v.parse::<i32>().ok())
+        .collect::<Vec<i32>>()[..]
+    {
+        [n, ] => (n, ),
+        _ => panic!("First line not valid"),
+    };
+
+    // Keep track of each number in the array and it's most recent index in the list
+    let mut indexes: BTreeMap<i32, usize> = BTreeMap::new();
+
+    let mut max = 1;
+    // Parse the array of numbers
+    let mut it: i32 = lines
+        .next()
+        .unwrap()
+        .unwrap()
+        .split_whitespace()
+        .filter_map(|v| v.parse::<i32>().ok())
+        .inspect(|v| println!("v{:?}", v))
+        .fold(1, |i, v| {
+            println!("i{}", i);
+            if let Some(x) = indexes.get(&v) {
+                println!("Some: {}@{}", v, x);
+            } else {
+                println!("Not some");
+            };
+            indexes.insert(v, i as usize);
+            i + 1
+        });
+    println!("It {:?}", it);
+    println!("Indexes {:?}", indexes);
+}
