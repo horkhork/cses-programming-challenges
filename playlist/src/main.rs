9ab878f221731ccfd19d96c3c14156ac2bc5e328
steve <ssosik@akamai.com>
2021-11-27 15:11:29 -0500

    Checkpoint

--- playlist/src/main.rs
+++ playlist/src/main.rs
@@ -1,13 +1,13 @@
+use std::cmp;
 use std::collections::BTreeMap;
 use std::io::{BufRead, BufReader};
-use std::cmp;
 
 fn main() {
     let input = BufReader::new(std::io::stdin());
     let mut lines = input.lines();
 
     // Parse the first line, unused
-    let (_n, ) = match lines
+    let (_n,) = match lines
         .next()
         .unwrap()
         .unwrap()
@@ -15,35 +15,35 @@ fn main() {
         .filter_map(|v| v.parse::<i32>().ok())
         .collect::<Vec<i32>>()[..]
     {
-        [n, ] => (n, ),
+        [n] => (n,),
         _ => panic!("First line not valid"),
     };
 
     // Keep track of each number in the array and it's most recent index in the list
     let mut indexes: BTreeMap<i32, usize> = BTreeMap::new();
 
-    let mut lb = 0;
-    let mut max = 1;
+    //let mut lb = 0;
+    //let mut max = 1;
     // Parse the array of numbers
-    let mut it: i32 = lines
+    let (_lb, _ub, max): (i32, i32, i32) = lines
         .next()
         .unwrap()
         .unwrap()
         .split_whitespace()
         .filter_map(|v| v.parse::<i32>().ok())
         //.inspect(|v| println!("v{:?}", v))
-        .fold(1, |i, v| {
+        .fold((0, 1, 0), |(mut lb, ub, mut max), v| {
             //println!("i{}", i);
-            if let Some(x) = indexes.get(&v) {
-                //println!("Some: {}@{}", v, x);
+            if indexes.contains_key(&v) {
                 lb += 1;
             } else {
-                //println!("Not some");
-                println!("range {}-{}={}", i, lb, i-lb);
+                max = cmp::max(max, ub - lb);
+                //println!("range {}-{}={}", i, lb, i-lb);
             };
-            indexes.insert(v, i as usize);
-            i + 1
+            indexes.insert(v, ub as usize);
+            (lb, ub + 1, max)
         });
-    println!("It {:?}", it);
-    println!("Indexes {:?}", indexes);
+    //println!("It {} {} {}", lb, ub, max);
+    //println!("Indexes {:?}", indexes);
+    println!("{}", max);
 }
