b5ebfafaecdcc48ebe4969dca745052b541e6605
steve <ssosik@akamai.com>
2021-11-29 09:32:49 -0500

    Playlist problem complete

--- playlist/src/main.rs
+++ playlist/src/main.rs
@@ -22,30 +22,28 @@ fn main() {
     // Keep track of each number in the array and it's most recent index in the list
     let mut indexes: BTreeMap<i32, usize> = BTreeMap::new();
 
-    //let mut lb = 0;
-    //let mut max = 1;
     // Parse the array of numbers
-    let (_lb, _ub, max): (i32, i32, i32) = lines
+    let (_, max): (i32, i32) = lines
         .next()
         .unwrap()
         .unwrap()
         .split_whitespace()
         .filter_map(|v| v.parse::<i32>().ok())
-        .inspect(|v| println!("v{:?}", v))
+        //.inspect(|v| println!("v{:?}", v))
         .enumerate()
-        .fold((0, 1, 0), |(mut lb, ub, mut max), (i, v)| {
-            if let Some(i) = indexes.get(&v) {
-                //lb += 1;
-                lb = *i as i32;
-                println!("lb incr {} i {}", lb, i);
-            } else {
-                max = cmp::max(max, ub - lb);
-                println!("range {}-{}={}", ub, lb, ub-lb);
+        .fold((0, 0), |(mut lb, mut max), (i, v)| {
+            // enumerated indexes are 1-based to be inclusive for the current item.
+            let i = i + 1;
+            //print!("{:>5}: value '{}'; ", i, v);
+            if let Some(prev) = indexes.get(&v) {
+                //print!("update lb from {} ", lb);
+                lb = cmp::max(lb, *prev as i32);
+                //print!("to {}; ", lb);
             };
-            indexes.insert(v, ub as usize);
-            (lb, ub + 1, max)
+            max = cmp::max(max, i as i32 - lb);
+            //println!("range {}-{}={}", i, lb, i as i32 - lb);
+            indexes.insert(v, i as usize);
+            (lb, max)
         });
-    //println!("It {} {} {}", lb, ub, max);
-    //println!("Indexes {:?}", indexes);
     println!("{}", max);
 }
